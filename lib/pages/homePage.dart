f53b8200b3a1f66b9044bd883b9fee0e6e471fde
renmurakami <autoeyy@gmail.com>
2022-06-02 09:05:05 +0900

    listview builder

--- lib/pages/homePage.dart
+++ lib/pages/homePage.dart
@@ -2,29 +2,35 @@ import 'package:flutter/material.dart';
 import 'package:flutter/src/foundation/key.dart';
 import 'package:flutter/src/widgets/framework.dart';
 
-class HomePage extends StatelessWidget {
+class HomePage extends StatefulWidget {
   const HomePage({Key? key}) : super(key: key);
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  final List<String> _todoItems = ["英語の課題", "牛乳を買う", "flutterを触る", "テスト"];
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text("hello"),
-      ),
-      body: ListView(
-        children: [
-          Card(
-            child: ListTile(
-              title: Text("sample"),
-            ),
-          ),
-          Card(
-            child: ListTile(
-              title: Text("sample"),
-            ),
-          )
-        ],
-      ),
-    );
+        appBar: AppBar(
+          title: Text("hello flutter"),
+        ),
+        body: ListView.builder(
+          itemCount: _todoItems.length,
+          itemBuilder: (BuildContext context, int index) {
+            return Card(
+              child: ListTile(
+                title: Text(_todoItems[index]),
+                trailing: IconButton(
+                  icon: const Icon(Icons.more_vert_rounded),
+                  onPressed: () {},
+                ),
+              ),
+            );
+          },
+        ));
   }
 }
